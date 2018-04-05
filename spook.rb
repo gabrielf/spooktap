b77a9cff504ba832b5a1c2605dca068307152713
Gabriel Falkenberg <gabriel@soundtrackyourbrand.com>
2018-04-05 11:44:13 +0200

    Describe how to listen to many files.

--- spook.rb
+++ spook.rb
@@ -14,6 +14,59 @@ class Spook < Formula
     bin.install "spook"
   end
 
+  def caveats
+    <<~EOS
+      To enable #{name} to watch more than 256 files you need to alter macOS's ulimits.
+
+        echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>limit.maxfiles</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>launchctl</string>
+      <string>limit</string>
+      <string>maxfiles</string>
+      <string>524288</string>
+      <string>524288</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>ServiceIPC</key>
+    <false/>
+  </dict>
+</plist>' | sudo tee /Library/LaunchDaemons/limit.maxfiles.plist
+        echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>limit.maxfiles</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>launchctl</string>
+      <string>limit</string>
+      <string>maxfiles</string>
+      <string>524288</string>
+      <string>524288</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>ServiceIPC</key>
+    <false/>
+  </dict>
+</plist>' | sudo tee /Library/LaunchDaemons/limit.maxproc.plist
+        sudo chown root:wheel /Library/LaunchDaemons/limit.max{files,proc}.plist
+        sudo launchctl load -w /Library/LaunchDaemons/limit.max{files,proc}.plist
+
+      You have to restart macOS for the above changes to have any effect!
+
+      For more details see: https://blog.dekstroza.io/ulimit-shenanigans-on-osx-el-capitan/
+    EOS
+  end
+
   test do
     system bin/"spook", "--version"
   end
