dabd69919fa25d773bd8736f8ed5472ed0c1bfad
Gabriel Falkenberg <gabriel@soundtrackyourbrand.com>
2018-04-17 09:54:46 +0200

    Upgrade version

--- spook.rb
+++ spook.rb
@@ -3,8 +3,8 @@ class Spook < Formula
   desc "Lightweight programmable evented utility based on LuaJIT and ljsyscall"
   homepage "https://github.com/johnae/spook"
   url "https://github.com/johnae/spook.git",
-      :tag => "0.9.3",
-      :revision => "c344112ed0f723f1834251dbbf36977d3fd128ea"
+      :tag => "0.9.4",
+      :revision => "2da76a101dd65398270bf9852696963e998ee354"
   head "https://github.com/johnae/spook.git"
 
   depends_on "cmake" => :build
